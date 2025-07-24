9d3cb07078d65fe72bec0626e8b97f122cb91a49
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-07-24 12:03:20 +0900

    ✨ feat: 커맨드 기반 명령 처리 시스템 구현
    - @mapping 애노테이션 정의
    - CommandController 클래스 추가
    - CommandDispatcher에서 명령어-메서드 자동 매핑 처리
    - CommandPreprocessor 생성 (클라이언트 쪽 전처리 로직용)
    - TCP 명령을 구조적으로 처리할 수 있도록 설계 개선

--- src/main/java/com/jaehyeoklim/tcp/chat/command/CommandController.java
+++ src/main/java/com/jaehyeoklim/tcp/chat/command/CommandController.java
@@ -1,6 +1,25 @@
 package main.java.com.jaehyeoklim.tcp.chat.command;
 
+import main.java.com.jaehyeoklim.tcp.chat.server.Session;
+import main.java.com.jaehyeoklim.tcp.chat.server.SessionManager;
+
+import java.io.IOException;
+
 public class CommandController {
 
+    private final SessionManager sessionManager;
 
+    public CommandController(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @Mapping("/message")
+    public void sendMessage(String message, Session session) throws IOException {
+        sessionManager.sendToAll(message);
+    }
+
+    @Mapping("/exit")
+    public void exit(String message,  Session session) throws IOException {
+        session.close();
+    }
 }
